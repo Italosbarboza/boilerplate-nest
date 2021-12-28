6542b0015c46bbce99b89fe3a212db8f077de3f8
Italo Barboza <italo_barboza@hotmail.com>
2021-12-28 14:49:43 -0300

    Continuacao do boilerplate, configuracao do usuario

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Query, Mutation, Resolver } from '@nestjs/graphql';
+import { CreateUserInput } from './dto/create-user.input';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
 @Resolver()
-export class UserResolver {}
+export class UserResolver {
+  constructor(private userServece: UserService) {}
+
+  @Query(() => [User])
+  async users(): Promise<User[]> {
+    const users = await this.userServece.findAllUsers();
+    return users;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Args('data') data: CreateUserInput): Promise<User> {
+    const user = await this.userServece.createUser(data);
+    return user;
+  }
+}
